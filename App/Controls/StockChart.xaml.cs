3dbf93b7b936962e10c5a22c5df11e037078d2e6
Scott Greenlay <scott@greenlay.net>
2015-12-30 09:51:29 -0800

    Refactored chart into a control

---
+++ App/Controls/StockChart.xaml.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+using Stockfighter.Api;
+
+namespace App.Controls
+{
+    public sealed partial class StockChart : UserControl
+    {
+        public Simulation Simulation { get; set; }
+
+        public StockChart()
+        {
+            this.InitializeComponent();
+        }
+
+        public void Invalidate()
+        {
+            Graph.Invalidate();
+        }
+
+        void Redraw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            if (Simulation == null)
+            {
+                args.DrawingSession.DrawText("Loading...", 20, 20, Colors.Black);
+            }
+            else
+            {
+                var yMin = Math.Min(Simulation.AskRange.Min.Price, Simulation.BidRange.Min.Price);
+                var yRange = Math.Max(Simulation.AskRange.Max.Price, Simulation.BidRange.Max.Price) - yMin;
+
+                var xMin = Simulation.Timeline.Start.Ticks;
+                var xRange = Simulation.Timeline.End.Ticks - xMin;
+
+                foreach (var entry in Simulation.cache.History)
+                {
+                    var x = ((float)(entry.Ts.Ticks - xMin) / (float)xRange) * sender.ActualWidth;
+
+                    if (entry.Bids != null)
+                    {
+                        foreach (var bid in entry.Bids)
+                        {
+                            var y = ((float)(bid.Price - yMin) / (float)yRange) * sender.ActualHeight;
+
+                            args.DrawingSession.FillCircle((float)x, (float)y, 2, Colors.Green);
+                        }
+                    }
+                    if (entry.Asks != null)
+                    {
+                        foreach (var ask in entry.Asks)
+                        {
+                            var y = ((float)(ask.Price - yMin) / (float)yRange) * sender.ActualHeight;
+
+                            args.DrawingSession.FillCircle((float)x, (float)y, 2, Colors.Red);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- App/MainPage.xaml.cs
+++ App/MainPage.xaml.cs
@@ -1,20 +1,15 @@
-﻿using Microsoft.Graphics.Canvas.UI.Xaml;
-using Stockfighter.Api;
+﻿
 using System;
-using System.Numerics;
 using Windows.ApplicationModel;
-using Windows.ApplicationModel.Core;
 using Windows.Storage;
-using Windows.UI;
-using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
+
+using Stockfighter.Api;
 
 namespace App
 {
     public sealed partial class MainPage : Page
     {
-        Simulation simulation { get; set; }
-
         public MainPage()
         {
             this.InitializeComponent();
@@ -28,49 +23,9 @@ namespace App
             var file = await folder.GetFileAsync("lvl4 2015-12-18 09-03-44.json");
 
             var json = await FileIO.ReadTextAsync(file);
-            simulation = new Simulation(json);
 
-            HistoryGraph.Invalidate();
-        }
-
-        void Redraw(CanvasControl sender, CanvasDrawEventArgs args)
-        {
-            if (simulation == null)
-            {
-                args.DrawingSession.DrawText("Loading...", 20, 20, Colors.Black);
-            }
-            else
-            {
-                var yMin = Math.Min(simulation.AskRange.Min.Price, simulation.BidRange.Min.Price);
-                var yRange = Math.Max(simulation.AskRange.Max.Price, simulation.BidRange.Max.Price) - yMin;
-
-                var xMin = simulation.Timeline.Start.Ticks;
-                var xRange = simulation.Timeline.End.Ticks - xMin;
-
-                foreach (var entry in simulation.cache.History)
-                {
-                    var x = ((float)(entry.Ts.Ticks - xMin) / (float)xRange) * sender.ActualWidth;
-
-                    if (entry.Bids != null)
-                    {
-                        foreach (var bid in entry.Bids)
-                        {
-                            var y = ((float)(bid.Price - yMin) / (float)yRange) * sender.ActualHeight;
-
-                            args.DrawingSession.FillCircle((float)x, (float)y, 2, Colors.Green);
-                        }
-                    }
-                    if (entry.Asks != null)
-                    {
-                        foreach (var ask in entry.Asks)
-                        {
-                            var y = ((float)(ask.Price - yMin) / (float)yRange) * sender.ActualHeight;
-
-                            args.DrawingSession.FillCircle((float)x, (float)y, 2, Colors.Red);
-                        }
-                    }
-                }
-            }
+            Chart.Simulation = new Simulation(json);
+            Chart.Invalidate();
         }
     }
 }
